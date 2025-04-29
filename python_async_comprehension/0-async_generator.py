c77ae08a053d12e4cc157e9fc31d44fbdf22dceb
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-04-29 09:28:33 +0200

    test checker

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module async_generator
+Génère des nombres aléatoires de manière asynchrone.
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None, None]:
+    """
+    Coroutine asynchrone générant 10 nombres
+    flottants aléatoires entre 0 et 10,
+    avec une pause d'une seconde entre chaque génération.
+    """
+
+    for _ in range(10):
+        i = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield i
